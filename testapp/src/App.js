09f2f1ea6bc8851c6eb0acb5336ae1f63267e49c
Sameem <sameemgujjar123@yahoo.com>
2021-05-03 16:58:00 +0500

    Added 05-03-2021 working code.

--- testapp/src/App.js
+++ testapp/src/App.js
@@ -2,17 +2,22 @@ import './App.css';
 import MyApp from './components/MyApp'
 
 const App = () => {
+  const HelloWorld = () => console.log("Hello World!")
+  
   return (
-    <>
+    <div className="App">
       <h1>Hello World</h1>
       <h2>jkajhjajkdwa</h2>
       <p>{new Date().toString()}</p>
       <ul>
         <li>awafaw</li>
       </ul>
-      <MyApp Name="Hazaifa" />
-    </>
+      <MyApp Name="Hazaifa" cityArray={4} HelloWorld={HelloWorld} />
+      {/* <MyApp Name="Ali" HelloWorld={HelloWorld} /> */}
+    </div>
   );
 }
 
+
 export default App;
+
--- testapp/src/components/MyApp.js
+++ testapp/src/components/MyApp.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import PropsExample from './PropsExample';
+import './styles.css';
+import natureImage from '../assets/images/nature.jpg';
+import PropType from 'prop-types';
 
 export default class MyApp extends Component {
   constructor(props) {
@@ -9,13 +12,33 @@ export default class MyApp extends Component {
     }
   }
 
+  static propTypes = {
+    Name : PropType.string,
+    cityArray: PropType.array
+  }
+
   render() {
+    this.props.HelloWorld()
     return (
-      <div>
+      <div id="container">
+        <img src={natureImage} alt="Nature Wallpaper" width="100%" height="100%"/>
+        <br/>
         <h1>{this.props.Name}</h1>
         <PropsExample Name={this.state.count}/>
         <h1>{this.state.count}</h1>
         <button onClick={() => this.setState({ count: this.state.count+1 })}>Increment</button>
+        <table style={{ color: "white", border: "1px solid"}} id="table">
+          <thead>
+            <tr>
+            <th>Sr. No.</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
